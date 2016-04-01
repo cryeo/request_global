26ff73d07880633b132746b542439dbb8941454d
Chaerim Yeo <yeochaerim@gmail.com>
2016-04-01 20:35:34 +0900

    Require railtie only when Rails::Railtie is defined

--- lib/request_global.rb
+++ lib/request_global.rb
@@ -1,6 +1,6 @@
 require "request_global/request_global"
 require "request_global/version"
 require "request_global/middleware"
-require "request_global/railtie"
+require "request_global/railtie" if defined?(Rails::Railtie)
 
 module RequestGlobal; end
