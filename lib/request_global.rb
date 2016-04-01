324ac3f37e3b391735f07a25601361226495442c
Chaerim Yeo <yeochaerim@gmail.com>
2016-04-01 20:20:36 +0900

    Add railtie and middleware to request_global

--- lib/request_global.rb
+++ lib/request_global.rb
@@ -1,6 +1,6 @@
-require "request_global/version"
 require "request_global/request_global"
+require "request_global/version"
+require "request_global/middleware"
+require "request_global/railtie"
 
-module RequestGlobal
-  # Your code goes here...
-end
+module RequestGlobal; end
