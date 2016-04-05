5d20eec371784fa9c6f4fcfc09b40a80362675dd
Chaerim Yeo <yeochaerim@gmail.com>
2016-04-05 22:52:11 +0900

    Modify spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,25 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'request_global'
+
+def storages
+  RequestGlobal.class_variable_get('@@storages')
+end
+
+def current_request
+  RequestGlobal.class_variable_get('@@current_request')
+end
+
+def test_request_id
+  "test_request_id"
+end
+
+class FakeApp
+  attr_reader :last_count
+
+  def call(env)
+    RequestGlobal.storage[:count] ||= 0
+    RequestGlobal.storage[:count] += 1
+    @last_count = RequestGlobal.storage[:count]
+    raise "fail" if env[:error]
+  end
+end
